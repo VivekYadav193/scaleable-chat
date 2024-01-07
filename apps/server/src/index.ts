9f3f270097eeb9ed8f6325d705d201d3747b0cc4
VivekYadav193 <vivekash03@gmail.com>
2024-01-07 13:34:23 +0530

    commit

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,23 @@
 import http from "http";
-
+import SocketService from "./services/socket";
+import { startMessageConsumer } from "./services/kafka";
 async function init() {
+  startMessageConsumer();
+  const socketService = new SocketService();
+
   const httpServer = http.createServer();
   const PORT = process.env.PORT ? process.env.PORT : 8000;
-  httpServer.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-  });
+
+  socketService.io.attach(httpServer);
+
+  httpServer.listen(PORT, () =>
+    console.log(`HTTP Server started at PORT:${PORT}`)
+  );
+
+  socketService.initListeners();
+
+ 
+
 }
 
 init();
